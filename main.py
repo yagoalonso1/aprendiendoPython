d14da78f1d2ba29b9534f76bdf2a3d54693b86f0
yagoalonso1 <yagoalonso3105@gmail.com>
2025-03-28 23:51:53 +0100

    ex2

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-#1 Imprime “Hola, mundo” en pantalla.
+#EJERCICIOS BASICOS
+
+#1. Imprime “Hola, mundo” en pantalla.
 
 print("Hola mundo")
+
+#2.	Solicita un número e imprímelo en pantalla.
+
+num1= int(input("Ingrese un numero: "))
+
+print("$ num1")
+
+#3.	Solicita dos números e imprime su suma.
+#4.	Solicita dos números e imprime su resta.
+#5.	Solicita dos números e imprime su multiplicación.
+#6.	Solicita dos números e imprime su división.
+#7.	Solicita un número e imprime si es par o impar.
+#8.	Solicita un número e imprime si es positivo, negativo o cero.
+#9.	Solicita dos números e imprime cuál es mayor.
+#10. Solicita tres números e imprime el mayor de los tres.
+#11. Solicita un número y muestra su tabla de multiplicar hasta el 10.
+#12. Convierte grados Celsius a Fahrenheit.
+#13. Convierte kilómetros a millas.
+#14. Calcula el área de un triángulo dados su base y altura.
+#15. Calcula el perímetro de un círculo dado su radio.
